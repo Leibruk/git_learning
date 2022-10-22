9250d4eb58aa6f3508f26a8cc64f17ac32233604
Leibruk <mateusz.kurbiel@gmail.com>
2022-10-22 11:08:42 +0200

    Add-new-def-flatten-list

--- list.py
+++ list.py
@@ -1,16 +1,14 @@
 
+def flatten_list(list):
+    flat_list = []
+    for element in list:
+        if type(element) is list:
+            for item in element:
+                flat_list.append(item)
+        else:
+            flat_list.append(element)
+    return flat_list
+
+
 list = [1, [2, [3, 4]], 5, 6, [[7, 8]], 9, [10]]
-
-# def sorted_list(x):
-#     y = []
-#     for _ in x:
-#         for i in sublist:
-#             y.append(i)
-
-
-def flatten(l):
-    return [item for sublist in l for item in sublist]
-
-# sorted_list(list)
-
-flatten(list)
+print(flatten_list(list))
