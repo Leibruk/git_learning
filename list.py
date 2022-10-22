b2a05a2e015caa1949bd9c843668f7d29ff31075
Leibruk <mateusz.kurbiel@gmail.com>
2022-10-22 12:05:38 +0200

    Clean-code-improved-flatten-list

--- list.py
+++ list.py
@@ -1,16 +1,4 @@
 
-# def flatten_list(list):
-#     flat_list = []
-#     for element in list:
-#         if type(element) is list:
-#             for item in element:
-#                 flat_list.append(item)
-#         else:
-#             flat_list.append(element)
-#     return flat_list
-
-# print(flatten_list(list))
-
 def flatten(x):
     result = []
     for el in x:
